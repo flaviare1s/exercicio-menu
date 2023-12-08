fb270b8a5236c43f572c4dc7e8d8f827006453e7
Flávia Reis <flaviarster@gmail.com>
2023-12-08 10:51:33 -0300

    Estou tentando fazer o segundo menu

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import './style.css'
 
-const burgerMenu = document.querySelector('#burger-icone')
+const burgerMenu = document.querySelector('#burger-icone1')
 const LinksMenuPrincipal = document.querySelectorAll('.link-menu-principal')
+const link2 = document.querySelector('#link2')
+const LinksMenuLink2 = document.querySelectorAll('.link-menu-link2')
+const burgerMenu2 = document.querySelector('#buger-icone2')
 
 burgerMenu.addEventListener('click', () => {
   for( const LinkMenuPrincipal of LinksMenuPrincipal) {
@@ -11,4 +14,15 @@ burgerMenu.addEventListener('click', () => {
       LinkMenuPrincipal.style.display = 'none'
     }
   }
+})
+
+link2.addEventListener('click', () => {
+  alert ('Teste')
+  // for ( const LinkMenuLink2 of LinksMenuLink2) {
+  //   if ( LinkMenuLink2.style.display === 'none') {
+  //     LinkMenuLink2.style.display === 'block'
+  //   } else {
+  //     LinkMenuLink2.style.display === 'none'
+  //   }
+  // }
 })
