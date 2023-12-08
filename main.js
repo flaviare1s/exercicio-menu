6d1f48611b10bf0e52347299810ccc3d747111a6
Flávia Reis <flaviarster@gmail.com>
2023-12-08 09:20:20 -0300

    criei burger menu

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 import './style.css'
 
+const burgerMenu = document.querySelector('#burger-icone')
+const LinksMenuPrincipal = document.querySelectorAll('.link-menu-principal')
+
+burgerMenu.addEventListener('click', () => {
+  for( const LinkMenuPrincipal of LinksMenuPrincipal) {
+    if ( LinkMenuPrincipal.style.display === 'none') {
+      LinkMenuPrincipal.style.display = 'block'
+    } else {
+      LinkMenuPrincipal.style.display = 'none'
+    }
+  }
+})
