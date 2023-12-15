a9d5d53760dab686ad345868f45dc1af551b6a3e
Flávia Reis <flaviarster@gmail.com>
2023-12-15 10:47:15 -0300

    Elaborando burger menu

--- burger.js
+++ burger.js
@@ -2,6 +2,10 @@ export function addBurgerMenuEvents() {
     const burgerMenuEl = document.querySelector('#burgerMenu')
     const menuOpenButtonEl = burgerMenuEl.querySelector('#menuOpenButton')
     const overlayEl = burgerMenuEl.querySelector('#menuOverlay')
+    const botao1El = burgerMenuEl.querySelector('#botao1')
+    const botao2El = burgerMenuEl.querySelector('#botao2')
+    const menuChild1El = burgerMenuEl.querySelector('#menuChild1')
+    const menuChild2El = burgerMenuEl.querySelector('#menuChild2')
   
     menuOpenButtonEl.addEventListener('click', () => {
       const menuBaseEl = burgerMenuEl.querySelector('#menuBase')
@@ -13,5 +17,13 @@ export function addBurgerMenuEvents() {
       const menuBaseEl = burgerMenuEl.querySelector('#menuBase')
       menuBaseEl.style.left = '-300px'
       overlayEl.classList.remove('active')
+    })
+
+    botao1El.addEventListener('click', () => {
+        menuChild1El.style.left = '0px'
+    })
+
+    botao2El.addEventListener('click', () => {
+        menuChild2El.style.left = '0px'
     })
   }
