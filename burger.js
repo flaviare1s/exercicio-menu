44d3f53fd5e53cd2cff46e05b95adede43a8450a
Flávia Reis <flaviarster@gmail.com>
2023-12-31 07:52:44 -0300

    Concluí o exercício de burger menu

--- burger.js
+++ burger.js
@@ -2,12 +2,7 @@ export function addBurgerMenuEvents() {
     const burgerMenuEl = document.querySelector('#burgerMenu')
     const menuOpenButtonEl = burgerMenuEl.querySelector('#menuOpenButton')
     const overlayEl = burgerMenuEl.querySelector('#menuOverlay')
-    const botao1El = burgerMenuEl.querySelector('#botao1')
-    const botao2El = burgerMenuEl.querySelector('#botao2')
-    const menuChild1El = burgerMenuEl.querySelector('#menuChild1')
-    const menuChild2El = burgerMenuEl.querySelector('#menuChild2')
-    const closeButton1El = burgerMenuEl.querySelector('#closeButton1')
-    const closeButton2El = burgerMenuEl.querySelector('#closeButton2')
+    
     const botoesPaisEl = Array.from(burgerMenuEl.querySelectorAll('.botaoPai'))
     const menuFilhoEl = Array.from(burgerMenuEl.querySelectorAll('.child'))
   
@@ -23,25 +18,16 @@ export function addBurgerMenuEvents() {
       overlayEl.classList.remove('active')
     })
 
-    botao1El.addEventListener('click', () => {
-        menuChild1El.style.left = '0px'
-    })
+    menuFilhoEl.forEach((childMenu, index) => {
+        const botaoPaiEl = botoesPaisEl[index]
+        const closeButtonEl = burgerMenuEl.querySelector(`#closeButton${index + 1}`)
 
-    botao2El.addEventListener('click', () => {
-        menuChild2El.style.left = '0px'
-    })
-
-    closeButton1El.addEventListener('click', () => {
-        menuChild1El.style.left = '-300px'
-    })
-
-    closeButton2El.addEventListener('click', () => {
-        menuChild2El.style.left = '-300px'
-    })
-
-    for (const botaoPaiEl of botoesPaisEl) {
         botaoPaiEl.addEventListener('click', () => {
-            menuFilhoEl.style.left = '0px'
+            childMenu.style.left = '0px'
         })
-    }
-  }
+
+        closeButtonEl.addEventListener('click', () => {
+            childMenu.style.left = '-300px'
+        })
+    })
+}
