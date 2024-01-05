715edb9d0b4af28e2ff4e717f4444251180fc587
Flávia Reis <flaviarster@gmail.com>
2024-01-05 09:36:54 -0300

    Estudando fetch

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 import { addBurgerMenuEvents } from './burger'
 import './style.css'
 
-function main() {
+
+async function fetchProducts() {
+  const products = await fetch('https://6597ee73668d248edf23ba81.mockapi.io/Product')
+  return products
+}
+
+
+async function main() {
   addBurgerMenuEvents()
+  const products = await fetchProducts()
+  console.log('### products:', products)
 }
 
 main()
