d6401233cc739840104e00f6dba9b36477ed7edc
Flávia Reis <flaviarster@gmail.com>
2024-01-12 10:20:34 -0300

    Criei a página de usuários

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { addBurgerMenuEvents } from './burger'
 import './style.css'
+import './users.css'
+import './products.css'
 
 
 async function fetchProducts() {
@@ -18,40 +20,75 @@ async function fetchUsers() {
 async function main() {
   addBurgerMenuEvents()
   const products = await fetchProducts()
-  console.log('### Products:', products)
+  console.log('Products:', products)
   const users = await fetchUsers()
-  console.log('### Users:', users)
+  console.log('Users:', users)
   
   const productsContainerEl = document.querySelector('#productsContainer')
-  for (const product of products) {
-    const productCard = document.createElement('div')
-    productCard.classList.add('product-card')
-
-    const productInformation = document.createElement('div')
-    productInformation.classList.add('product-information')
-
-    const imageContainer = document.createElement('div')
-    imageContainer.classList.add('image-container')
-
-    const imageEl = document.createElement('img')
-    imageEl.src = product.image
-    imageEl.classList.add('image')
-
-    const productNameEl = document.createElement('div')
-    productNameEl.classList.add('product-name')
-    productNameEl.innerHTML = product.name
-    
-    const productPriceEl = document.createElement('div')
-    productPriceEl.classList.add('product-price')
-    productPriceEl.innerHTML = 'R$' + ' ' + product.price
-
-    productsContainerEl.appendChild(productCard)
-    productCard.appendChild(imageContainer)
-    imageContainer.appendChild(imageEl)
-    productCard.appendChild(productInformation)
-    productInformation.appendChild(productNameEl)
-    productInformation.appendChild(productPriceEl)
+  if(productsContainerEl) {
+    for (const product of products) {
+      const productCard = document.createElement('div')
+      productCard.classList.add('product-card')
+  
+      const productInformation = document.createElement('div')
+      productInformation.classList.add('product-information')
+  
+      const imageContainer = document.createElement('div')
+      imageContainer.classList.add('image-container')
+  
+      const imageEl = document.createElement('img')
+      imageEl.src = product.image
+      imageEl.classList.add('image')
+  
+      const productNameEl = document.createElement('div')
+      productNameEl.classList.add('product-name')
+      productNameEl.innerHTML = product.name
+      
+      const productPriceEl = document.createElement('div')
+      productPriceEl.classList.add('product-price')
+      productPriceEl.innerHTML = 'R$' + ' ' + product.price
+  
+      productsContainerEl.appendChild(productCard)
+      productCard.appendChild(imageContainer)
+      imageContainer.appendChild(imageEl)
+      productCard.appendChild(productInformation)
+      productInformation.appendChild(productNameEl)
+      productInformation.appendChild(productPriceEl)
+    }
+  
   }
+ 
+  const usersContainerEl = document.querySelector('#usersContainer')
+
+  if(usersContainerEl) {
+    for (const user of users) {
+      const userCard = document.createElement('div')
+      userCard.classList.add('user-card')
+  
+      const userInformation = document.createElement('div')
+      userInformation.classList.add('user-information')
+  
+      const userImageContainer = document.createElement('div')
+      userImageContainer.classList.add('user-image-container')
+  
+      const userImageEl = document.createElement('img')
+      userImageEl.src = user.avatar
+      userImageEl.classList.add('user-image')
+  
+      const userNameEl = document.createElement('div')
+      userNameEl.classList.add('user-name')
+      userNameEl.innerHTML = user.name
+  
+  
+      usersContainerEl.appendChild(userCard)
+      userCard.appendChild(userInformation)
+      userCard.appendChild(userImageContainer)
+      userImageContainer.appendChild(userImageEl)
+      userInformation.appendChild(userNameEl)
+    }
+  } 
+  
+
 }
 
 main()
